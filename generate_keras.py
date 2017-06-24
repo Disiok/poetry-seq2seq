af6e977017ebb68f7aa162608b50c7b6fb532f67
Simon Suo <simonsdsuo@gmail.com>
2017-06-24 17:06:31 -0400

    WIP

--- generate_keras.py
+++ generate_keras.py
@@ -59,6 +59,7 @@ class Generator:
         self.model.compile(loss='mse', optimizer='rmsprop')
             
         if os.path.exists(_model_path):
+            print 'Model exists, loading.'
             self.model.load_weights(_model_path)
    
     def train(self, 
@@ -85,7 +86,7 @@ class Generator:
         outputs = []
         for index, keyword in enumerate(keywords):
             # prepare input
-            input_ch = previous_sentences + keyword
+            input_ch = keyword + previous_sentences 
             input_int = [self.ch2int[ch] for ch in input_ch]
             input_int_pad = pad_to(input_int, _INPUT_LENGTH, _VOCAB_SIZE - 1)
             input_embedded = [map(lambda x: self.embedding[x], input_int_pad)]
@@ -98,7 +99,7 @@ class Generator:
             output_list = map(lambda word_vec: self.w2v_model.most_similar(positive=[word_vec], topn=1)[0][0], 
                             output_embedded[0])
             output_ch = ''.join(output_list)
-            previous_sentences += output_ch
+            previous_sentences += output_ch[:7]
 
             outputs.append(output_ch)
 
