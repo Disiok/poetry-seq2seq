23e38c64e289f22f679512b5954fef41e3e696c9
Simon Suo <simonsdsuo@gmail.com>
2017-09-08 02:13:31 -0400

    Everything works

--- webapp/public/chartAjax.js
+++ webapp/public/chartAjax.js
@@ -5,7 +5,7 @@ var LIGHTCORAL = 'rgba(240,128,128, 0.5)'
 var LIGHTBROWN = 'rgba(206, 181, 167, 0.5)'
 
 var models_by_author = {
-  'Computer': ['seq2seq', 'vrae'],
+  'Computer': ['vrae', 'seq2seq'],
   'Human': ['human']
 }
 
@@ -49,16 +49,16 @@ $(document).ready(function() {
     data: {
       datasets: [{
         data: [],
-        backgroundColor: [LIGHTCORAL, MEDIUMAQUAMARINE],
-        labels: 'Computer: vrae'
+        backgroundColor: [LIGHTCORAL, MEDIUMAQUAMARINE, LIGHTCORAL, MEDIUMAQUAMARINE],
       }, {
         data: [],
-        backgroundColor: [LIGHTCORAL, MEDIUMAQUAMARINE],
-        labels: 'Computer: seq2seq'
+        backgroundColor: [LIGHTCORAL, MEDIUMAQUAMARINE, LIGHTCORAL, MEDIUMAQUAMARINE],
       }],
       labels: [
-        'False guess as human',
-        'Correct guess as computer'
+        'False guess as human: vrae',
+        'Correct guess as computer: vrae',
+        'False guess as human: seq2seq',
+        'Correct guess as computer: seq2seq'
       ]
     },
     options: chartOption
@@ -125,23 +125,21 @@ $(document).ready(function() {
 function getTotals() {
   $.ajax({url: "/chartInfo", success: function(result){
     for (var author of ['Computer', 'Human']) {
-      
-
       models = models_by_author[author]
 
       var pieChart = charts['Guess'][author];
-      for (var model of models.entries()) {
-        var pieData = pieChart.data.datasets[model[0]].data;
-        for (var guessedAuthor of ['Computer', 'Human'].entries()) {
-          pieData[guessedAuthor[0]] = result[author][model[1]]['Guess'][guessedAuthor[1]];
+      for (var [modeInd, model] of models.entries()) {
+        var pieData = pieChart.data.datasets[modeInd].data;
+        for (var [guessedAuthorInd, guessedAuthor] of ['Computer', 'Human'].entries()) {
+          pieData[guessedAuthorInd + 2 * modeInd] = result[author][model]['Guess'][guessedAuthor];
         }
       }
 
       var radarChart = charts['Score'][author];
-      for (var model of models.entries()) {
-        var radarData = radarChart.data.datasets[model[0]].data;
-        for (var scoreDimension of ['Readability', 'Consistency', 'Poeticness', 'Evocative', 'Overall'].entries()) {
-          radarData[scoreDimension[0]] = result[author][model[1]]['Score'][scoreDimension[1]];
+      for (var [modelInd, model] of models.entries()) {
+        var radarData = radarChart.data.datasets[modelInd].data;
+        for (var [dimInd, scoreDimension] of ['Readability', 'Consistency', 'Poeticness', 'Evocative', 'Overall'].entries()) {
+          radarData[dimInd] = result[author][model]['Score'][scoreDimension];
         }
       }
 
