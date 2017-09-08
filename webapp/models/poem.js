1c7eaa751e072182e33b6bb62da08531721d67ca
Disiok <simonsdsuo@gmail.com>
2017-09-07 23:56:21 -0400

    Add new samples

--- webapp/models/poem.js
+++ webapp/models/poem.js
@@ -2,8 +2,19 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var PoemSchema = new Schema({
-  content: { type: String, default:''},
-  author: {type: String, required: true, enum:['Human', 'Computer']}
+  content: {
+  	type: [String], 
+  	default:''
+  },
+  author: {
+  	type: String, 
+  	required: true, 
+  	enum:['Human', 'Computer']
+  },
+  model: {
+  	type: String, 
+  	default: ''
+  }
 });
 
 module.exports = mongoose.model('Poem', PoemSchema);
